f9e1d4776b2c444a7673383cc8e78f6f60d6bb8b
Crowdbotics <team@crowdbotics.com>
2021-08-08 23:40:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "icy_bonus_29647Identifier",
+  url: "https://icy_bonus_29647Identifier.botics.co",
+  api: "https://icy_bonus_29647Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
